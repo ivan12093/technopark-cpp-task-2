6531fb7b4ac2d2bcff47a63756c9060c8567e2fd
Ivan Bulgakov <wanjasoldier@gmail.com>
2022-10-23 04:02:50 +0300

    Added some opearators.

--- include/matrix.h
+++ include/matrix.h
@@ -9,15 +9,30 @@ private:
     T** data = nullptr;
     size_t rows = 0;
     size_t cols = 0;
-    void swap(Matrix& other) noexcept;
+    friend void swap(Matrix& a, Matrix& b) noexcept;
 public:
     Matrix() = delete;
     Matrix(size_t rows, size_t cols);
+    Matrix(T** data, size_t rows, size_t cols);
     ~Matrix();
+    Matrix(const Matrix& other);
+    Matrix(Matrix&& other) noexcept;
     Matrix& operator=(const Matrix& other);
-    Matrix(Matrix& other);
-    Matrix& operator=(Matrix&& other);
+    Matrix& operator=(Matrix&& other) noexcept;
+
+    Matrix transposed();
+    Matrix inversed();
+    T determinant();
+
+    Matrix operator-(const Matrix& other);
+    Matrix operator+(const Matrix& other);
+    Matrix operator*(const Matrix& other);
+
+    friend Matrix operator*(const Matrix& a, const T& b);
+    Matrix operator*(const T& other);
+    Matrix& operator*=(const T& other);
 };
+
 
 
 #endif //TECHNOPARK_CPP_TASK_2_MATRIX_H
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -14,7 +14,7 @@ Matrix<T>::Matrix(size_t _rows, size_t _cols): rows(_rows), cols(_cols) {
 }
 
 template<class T>
-Matrix<T>::Matrix(Matrix<T>& other) {
+Matrix<T>::Matrix(const Matrix<T>& other) {
     Matrix(other.rows, other.cols);
     for (size_t i = 0; i < rows; ++i)
         std::copy(other.data[i], other.data[i] + cols, data[i]);
@@ -35,14 +35,104 @@ Matrix<T>& Matrix<T>::operator=(const Matrix<T>& other) {
     if (this == &other)
         return *this;
     Matrix<T> tmp(other);
-    swap(tmp);
+    swap(*this, tmp);
     return *this;
 }
 
 template<class T>
-void Matrix<T>::swap(Matrix<T> &other) noexcept {
-    std::swap(data, other.data);
-    std::swap(rows, other.rows);
-    std::swap(cols, other.cols);
+void swap(Matrix<T>& a, Matrix<T> &b) noexcept {
+    std::swap(a.data, b.data);
+    std::swap(a.rows, b.rows);
+    std::swap(a.cols, b.cols);
 }
 
+template<class T>
+Matrix<T>& Matrix<T>::operator=(Matrix&& other) noexcept {
+    if (this == &other)
+        return *this;
+    ~Matrix();
+    swap(*this, other);
+}
+
+template<class T>
+Matrix<T>::Matrix(Matrix<T> &&other) noexcept {
+    if (this == &other)
+        return;
+    ~Matrix();
+    swap(*this, other);
+}
+
+template<class T>
+Matrix<T>::Matrix(T **_data, size_t rows, size_t cols) {
+    Matrix(rows, cols);
+    for (size_t i = 0; i < rows; ++i)
+        std::copy(_data[i], _data[i] + cols, data[i]);
+}
+
+template<class T>
+Matrix<T> Matrix<T>::transposed() {
+    Matrix result(rows, cols);
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            result.data[j][i] = data[i][j];
+    return result;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator-(const Matrix &other) {
+    assert(rows == other.rows);
+    assert(cols == other.cols);
+    Matrix result(rows, cols);
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            result[i][j] = data[i][j] - other.data[i][j];
+    return result;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator+(const Matrix &other) {
+    assert(rows == other.rows);
+    assert(cols == other.cols);
+    Matrix result(rows, cols);
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            result[i][j] = data[i][j] + other.data[i][j];
+    return result;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator*(const Matrix &other) {
+    assert(rows == other.rows);
+    assert(cols == other.cols);
+    Matrix result(rows, cols);
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            result[i][j] = data[i][j] * other.data[i][j];
+    return result;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator*(const T &other) {
+    Matrix result(*this);
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            result[i][j] = data[i][j] * other;
+    return result;
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator*=(const T& other) {
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; i < cols; ++j)
+            data[i][j] = data[i][j] * other;
+    return *this;
+}
+
+template<class T>
+Matrix<T> operator*(const Matrix<T> &a, const T &b) {
+    Matrix result(a.rows, a.cols);
+    for (size_t i = 0; i < a.rows; ++i)
+        for (size_t j = 0; j < a.cols; ++j)
+            result.data[i][j] = a.data[i][j] * b;
+    return result;
+}
