d275c7c5c9a02890045ebe5ee7a94d97b27eeb30
Ivan <wanjasoldier@gmail.com>
2022-10-24 05:39:03 +0300

    Added tests on vector and matrix.

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -9,15 +9,14 @@ using std::size_t;
 template<class T>
 class Matrix {
 private:
-    Vector<Vector<T>> data = nullptr;
+    Vector<Vector<T>> data;
     size_t rows = 0;
     size_t cols = 0;
 public:
-    Matrix() = delete;
+    Matrix() = default;
     Matrix(size_t _rows, size_t _cols);
     Matrix(T** _data, size_t _rows, size_t _cols);
     explicit Matrix(Vector<Vector<T>> vec_of_vec);
-    Matrix(Vector<T>* vectors, size_t size);
     Matrix(std::initializer_list<Vector<T>> initializerList);
     Matrix(std::initializer_list<std::initializer_list<T>> initializerList);
 
@@ -29,7 +28,7 @@ public:
 
     Matrix transposed();
     //Matrix inversed();
-    T determinant();
+    T determinant() const;
 
     Matrix operator-(const Matrix& other);
     Matrix operator+(const Matrix& other);
@@ -49,5 +48,248 @@ public:
 
     void swap(Matrix& other) noexcept;
 };
+
+#include <cassert>
+#include <algorithm>
+
+template<class T>
+Matrix<T>::Matrix(size_t _rows, size_t _cols): rows(_rows), cols(_cols) {
+    assert(rows != 0);
+    assert(cols != 0);
+    data = Vector<Vector<T>>(rows);
+    for (size_t i = 0 ; i < rows; ++i)
+        data[i] = Vector<T>(cols);
+}
+
+template<class T>
+Matrix<T>::Matrix(const Matrix<T>& other) {
+    Matrix(other.rows, other.cols);
+    for (size_t i = 0; i < rows; ++i)
+        std::copy(other.data[i], other.data[i] + cols, data[i]);
+}
+
+template<class T>
+Matrix<T>::~Matrix() {
+    rows = 0;
+    cols = 0;
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator=(const Matrix<T>& other) {
+    if (this == &other)
+        return *this;
+    Matrix<T> tmp(other);
+    swap(*this, tmp);
+    return *this;
+}
+
+template<class T>
+void Matrix<T>::swap(Matrix<T> &other) noexcept {
+std::swap(data, other.data);
+std::swap(rows, other.rows);
+std::swap(cols, other.cols);
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator=(Matrix&& other) noexcept {
+    if (this == &other)
+        return *this;
+    swap(*this, other);
+}
+
+template<class T>
+Matrix<T>::Matrix(Matrix<T> &&other) noexcept {
+    if (this == &other)
+        return;
+    swap(*this, other);
+}
+
+template<class T>
+Matrix<T>::Matrix(T **_data, size_t _rows, size_t _cols) {
+    assert(_rows != 0);
+    assert(_rows != 0);
+    data = Vector<Vector<T>>(rows);
+    rows = _rows;
+    cols = _cols;
+    for (size_t i = 0; i < rows; ++i)
+        data[i] = Vector<T>(_data[i], cols);
+}
+
+template<class T>
+Matrix<T> Matrix<T>::transposed() {
+    Matrix result(rows, cols);
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            result.data[j][i] = data[i][j];
+    return result;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator-(const Matrix &other) {
+    assert(rows == other.rows);
+    assert(cols == other.cols);
+    Matrix result(rows, cols);
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            result[i][j] = data[i][j] - other.data[i][j];
+    return result;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator+(const Matrix &other) {
+    assert(rows == other.rows);
+    assert(cols == other.cols);
+    Matrix result(rows, cols);
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            result[i][j] = data[i][j] + other.data[i][j];
+    return result;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator*(const Matrix &other) {
+    assert(rows == other.rows);
+    assert(cols == other.cols);
+    Matrix result(rows, cols);
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            result[i][j] = data[i][j] * other.data[i][j];
+    return result;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator*(const T &other) {
+    Matrix result(*this);
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            result[i][j] = data[i][j] * other;
+    return result;
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator*=(const T& other) {
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; i < cols; ++j)
+            data[i][j] = data[i][j] * other;
+    return *this;
+}
+
+template<class T>
+Matrix<T> operator*(const Matrix<T> &a, const T &b) {
+    Matrix result(a.rows, a.cols);
+    for (size_t i = 0; i < a.rows; ++i)
+        for (size_t j = 0; j < a.cols; ++j)
+            result.data[i][j] = a.data[i][j] * b;
+    return result;
+}
+
+template<class T>
+T Matrix<T>::determinant() const {
+    assert(rows == cols);
+    T n1;
+    T n2;
+    T det(1);
+    T total(1);
+    size_t index;
+    size_t _size = size().first;
+
+    Vector<T> temp(_size + 1);
+    for (size_t i = 0; i < _size; ++i) {
+        index = i;
+        while (index < _size && data[index][i] == T(0))
+            ++index;
+        if (index == _size)
+            continue;
+        if (index != i) {
+            for (size_t j = 0; j < _size; ++j)
+                std::swap(data[index][j], data[i][j]);
+            det = det * ((index - i) % 2 == 0 ? T(1) : T(-1));
+        }
+        for (size_t j = 0; j < _size; ++j)
+            temp[j] = data[i][j];
+        for (size_t j = i + 1; j < _size; ++j) {
+            n1 = temp[i];
+            n2 = data[j][i];
+            for (size_t k = 0; k < _size; ++k)
+                data[j][k] = (n1 * data[j][k]) - (n2 * temp[k]);
+            total *= n1;
+        }
+    }
+    for (size_t i = 0; i < _size; ++i)
+        det *= data[i][i];
+    return det / total;
+}
+
+template<class T>
+Vector<T> Matrix<T>::getDiag() const {
+    size_t elems_in_diag = std::min(rows, cols);
+    Vector<T> result(elems_in_diag);
+    for (size_t i = 0; i < elems_in_diag; ++i)
+        result[i] = data[i][i];
+}
+
+template<class T>
+Matrix<T>::Matrix(Vector<Vector<T>> vec_of_vec) {
+    if (vec_of_vec[0].get_format() == Row) {
+        data = vec_of_vec;
+        rows = vec_of_vec.size();
+        cols = vec_of_vec[0].size();
+        return;
+    }
+    rows = vec_of_vec[0].size();
+    cols = vec_of_vec.size();
+    data = Vector<Vector<T>>(rows);
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; i < rows; ++j)
+            data[i][j] = vec_of_vec[j][i];
+}
+
+template<class T>
+Matrix<T>::Matrix(std::initializer_list<Vector<T>> initializerList) {
+    assert(initializerList.size() != 0);
+    rows = initializerList.size();
+    cols = initializerList.begin()->size();
+    data = Vector<Vector<T>>(rows);
+    for (auto it_src = initializerList.begin(),
+                 it_dst = data.begin(); it_src < initializerList.end(); ++it_src, ++it_dst)
+        *it_dst = *it_src;
+}
+
+template<class T>
+Matrix<T>::Matrix(std::initializer_list<std::initializer_list<T>> initializerList) {
+    assert(initializerList.size() != 0);
+    rows = initializerList.size();
+    cols = initializerList.begin()->size();
+    assert(cols != 0);
+    data = Vector<Vector<T>>(rows);
+    auto it_dst = data.begin();
+    for (auto it_src = initializerList.begin(); it_src < initializerList.end(); ++it_src, ++it_dst) {
+        assert(it_src->size() == cols);
+        *it_dst = *it_src;
+    }
+}
+
+template<class T>
+Vector<T> Matrix<T>::getColumn(size_t idx) const {
+    Vector<T> result(rows, Column);
+    for (size_t i = 0; i < rows; ++i)
+        result[i] = data[i][idx];
+    return result;
+}
+
+template<class T>
+Vector<T> Matrix<T>::operator[](size_t idx) const {
+    return data[idx];
+}
+
+template<class T>
+Vector<T>& Matrix<T>::operator[](size_t idx) {
+    return data[idx];
+}
+
+template<class T>
+std::pair<size_t, size_t> Matrix<T>::size() const {
+    return std::make_pair(rows, cols);
+}
 
 #endif //TECHNOPARK_CPP_TASK_2_MATRIX_HPP
--- include/vector.hpp
+++ include/vector.hpp
@@ -1,8 +1,8 @@
 #ifndef TECHNOPARK_CPP_TASK_2_VECTOR_HPP
 #define TECHNOPARK_CPP_TASK_2_VECTOR_HPP
 
-using std::size_t;
 #include <utility>
+using std::size_t;
 
 using Format = enum Format_{Row, Column};
 
@@ -13,7 +13,7 @@ private:
     size_t _size = 0;
     Format _format = Row;
 public:
-    Vector() = delete;
+    Vector() = default;
     explicit Vector(size_t size, Format format = Row);
     Vector(T* data, size_t size, Format format = Row);
     Vector(std::initializer_list<T> initializerList, Format = Row);
@@ -51,11 +51,235 @@ public:
 
     size_t size() const;
     void set_format(Format format);
-    Format get_format();
+    Format get_format() const;
     void swap(Vector& other) noexcept;
 
-    T* begin();
-    T* end();
+    T* begin() const;
+    T* end() const;
 };
+
+#include <algorithm>
+#include <cassert>
+
+template<class T>
+Vector<T>::Vector(T* data, size_t size, Format format): Vector(size, format) {
+    assert(size != 0);
+    std::copy(data, data + size, _data);
+}
+
+template<class T>
+Vector<T>::Vector(size_t size, Format format): _size(size), _format(format) {
+    assert(size != 0);
+    _data = new T[size]();
+}
+
+template<class T>
+Vector<T>::~Vector() {
+    delete[] _data;
+    _data = nullptr;
+    _size = 0;
+}
+
+template<class T>
+Vector<T>::Vector(std::initializer_list<T> initializerList, Format format): Vector(initializerList.size(), format) {
+    assert(initializerList.size() != 0);
+    std::copy(initializerList.begin(), initializerList.end(), _data);
+}
+
+template<class T>
+Vector<T>::Vector(const Vector &other): Vector(other._size, other._format) {
+    std::copy(other.begin(), other.end(), _data);
+}
+
+template<class T>
+T* Vector<T>::begin() const {
+    return _data;
+}
+
+template<class T>
+T* Vector<T>::end() const {
+    return _data + _size;
+}
+
+template<class T>
+void Vector<T>::swap(Vector &other) noexcept {
+std::swap(_data, other._data);
+std::swap(_size, other._size);
+std::swap(_format, other._format);
+}
+
+template<class T>
+Vector<T>::Vector(Vector &&other) noexcept {
+if (this != &other)
+swap(other);
+}
+
+template<class T>
+Vector<T>& Vector<T>::operator=(const Vector &other) {
+    if (this == &other)
+        return *this;
+    Vector<T> tmp(other);
+    swap(tmp);
+    return *this;
+}
+
+template<class T>
+Vector<T>& Vector<T>::operator=(Vector &&other) noexcept {
+    if (this != &other)
+        swap(other);
+    return *this;
+}
+
+template<class T>
+Vector<T> Vector<T>::operator-(const Vector &other) const {
+    assert(size() == other.size());
+    assert(get_format() == other.get_format());
+    Vector<T> result(size(), _format);
+    for (size_t i = 0; i < size(); ++i)
+        result[i] = _data[i] - other[i];
+    return result;
+}
+
+template<class T>
+Vector<T> Vector<T>::operator+(const Vector &other) const {
+    assert(size() == other.size());
+    assert(get_format() == other.get_format());
+    Vector<T> result(size(), _format);
+    for (size_t i = 0; i < size(); ++i)
+        result[i] = _data[i] + other[i];
+    return result;
+}
+
+template<class T>
+Vector<T> Vector<T>::operator*(const Vector &other) const {
+    assert(size() == other.size());
+    assert(get_format() == other.get_format());
+    Vector<T> result(size(), _format);
+    for (size_t i = 0; i < _size; ++i)
+        result[i] = _data[i] * other[i];
+    return result;
+}
+
+template<class T, class Format>
+Vector<T> operator*(const T& a, const Vector<T>& b) {
+    Vector<T> result(b.size(), b.get_format());
+    for (size_t i = 0; i < b.size(); ++i)
+        result[i] = b[i] * a;
+    return result;
+}
+
+template<class T>
+Vector<T> Vector<T>::operator*(const T &elem) const {
+    Vector<T> result(size(), get_format());
+    for (size_t i = 0; i < size(); ++i)
+        result[i] = _data[i] * elem;
+    return result;
+}
+
+template<class T>
+Vector<T>& Vector<T>::operator*=(const T &elem) {
+    for (size_t i = 0; i < size(); ++i)
+        _data[i] = _data[i] * elem;
+    return *this;
+}
+
+template<class T>
+Vector<T> Vector<T>::operator-(const T &elem) const {
+    Vector<T> result(size(), get_format());
+    for (size_t i = 0; i < size(); ++i)
+        result[i] = _data[i] - elem;
+    return result;
+}
+
+template<class T>
+Vector<T> Vector<T>::operator+(const T &elem) const {
+    Vector<T> result(size(), get_format());
+    for (size_t i = 0; i < size(); ++i)
+        result[i] = _data[i] + elem;
+    return result;
+}
+
+template<class T>
+Vector<T>& Vector<T>::operator-=(const Vector &other) {
+    assert(size() == other.size());
+    assert(get_format() == other.get_format());
+    for (size_t i = 0; i < size(); ++i)
+        _data[i] = _data[i] - other[i];
+    return *this;
+}
+
+template<class T>
+Vector<T>& Vector<T>::operator+=(const Vector &other) {
+    assert(size() == other.size());
+    assert(get_format() == other.get_format());
+    for (size_t i = 0; i < size(); ++i)
+        _data[i] = _data[i] + other[i];
+    return *this;
+}
+
+template<class T>
+Vector<T>& Vector<T>::operator*=(const Vector &other) {
+    assert(size() == other.size());
+    assert(get_format() == other.get_format());
+    for (size_t i = 0; i < size(); ++i)
+        _data[i] = _data[i] * other[i];
+    return *this;
+}
+
+template<class T>
+Vector<T>& Vector<T>::operator-=(const T &elem) {
+    for (size_t i = 0; i < size(); ++i)
+        _data[i] = _data[i] - elem;
+    return *this;
+}
+
+template<class T>
+Vector<T>& Vector<T>::operator+=(const T &elem) {
+    for (size_t i = 0; i < size(); ++i)
+        _data[i] = _data[i] + elem;
+    return *this;
+}
+
+template<class T>
+Vector<T> operator+(const T& a, const Vector<T>& b) {
+    Vector<T> result(b.size(), b.get_format());
+    for (size_t i = 0; i < b.size(); ++i)
+        result[i] = b[i] + a;
+    return result;
+}
+
+template<class T>
+Vector<T> operator-(const T& a, const Vector<T>& b) {
+    Vector<T> result(b.size(), b.get_format());
+    for (size_t i = 0; i < b.size(); ++i)
+        result[i] = a - b[i];
+    return result;
+}
+
+template<class T>
+T Vector<T>::operator[](size_t idx) const {
+    return _data[idx];
+}
+
+template<class T>
+T& Vector<T>::operator[](size_t idx) {
+    return _data[idx];
+}
+
+template<class T>
+size_t Vector<T>::size() const {
+    return _size;
+}
+
+template<class T>
+void Vector<T>::set_format(Format format) {
+    _format = format;
+}
+
+template<class T>
+Format Vector<T>::get_format() const {
+    return _format;
+}
+
 
 #endif //TECHNOPARK_CPP_TASK_2_VECTOR_HPP
