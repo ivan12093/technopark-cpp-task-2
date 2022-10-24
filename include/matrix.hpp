1e1a7325981d1a9e4cbb732f49d1dd6a9946f7d1
Ivan <wanjasoldier@gmail.com>
2022-10-24 09:33:44 +0300

    Added all tests and almost all functionality.

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -12,6 +12,8 @@ private:
     Vector<Vector<T>> data;
     size_t rows = 0;
     size_t cols = 0;
+    T _determinant(const Matrix& mtx) const;
+    Matrix downsized(size_t row, size_t col) const;
 public:
     Matrix() = default;
     Matrix(size_t _rows, size_t _cols);
@@ -34,10 +36,30 @@ public:
     Matrix operator+(const Matrix& other);
     Matrix operator*(const Matrix& other);
 
-    template<class U>
-    friend Matrix<U> operator*(const Matrix<U>& a, const U& b);
+    Matrix& operator-=(const Matrix& other);
+    Matrix& operator+=(const Matrix& other);
+    Matrix& operator*=(const Matrix& other);
+
+    Matrix operator-(const T& other);
+    Matrix operator+(const T& other);
     Matrix operator*(const T& other);
+
+    template<class U>
+    friend Matrix<U> operator*(const U& a, const Matrix<U>& b);
+    template<class U>
+    friend Matrix<U> operator-(const U& a, const Matrix<U>& b);
+    template<class U>
+    friend Matrix<U> operator+(const U& a, const Matrix<U>& b);
+
     Matrix& operator*=(const T& other);
+    Matrix& operator+=(const T& other);
+    Matrix& operator-=(const T& other);
+
+    Matrix operator*(const Vector<T>& other);
+    template<class U>
+    friend Matrix<U> operator*(const Vector<U>& a, const Matrix<U>& b);
+
+    Matrix dot(const Matrix& other);
 
     Vector<T> getDiag() const;
     Vector<T> getColumn(size_t idx) const;
@@ -63,9 +85,9 @@ Matrix<T>::Matrix(size_t _rows, size_t _cols): rows(_rows), cols(_cols) {
 
 template<class T>
 Matrix<T>::Matrix(const Matrix<T>& other) {
-    Matrix(other.rows, other.cols);
-    for (size_t i = 0; i < rows; ++i)
-        std::copy(other.data[i], other.data[i] + cols, data[i]);
+    rows = other.rows;
+    cols = other.cols;
+    data = other.data;
 }
 
 template<class T>
@@ -79,29 +101,30 @@ Matrix<T>& Matrix<T>::operator=(const Matrix<T>& other) {
     if (this == &other)
         return *this;
     Matrix<T> tmp(other);
-    swap(*this, tmp);
+    swap(tmp);
     return *this;
 }
 
 template<class T>
 void Matrix<T>::swap(Matrix<T> &other) noexcept {
-std::swap(data, other.data);
-std::swap(rows, other.rows);
-std::swap(cols, other.cols);
+    std::swap(data, other.data);
+    std::swap(rows, other.rows);
+    std::swap(cols, other.cols);
 }
 
 template<class T>
 Matrix<T>& Matrix<T>::operator=(Matrix&& other) noexcept {
     if (this == &other)
         return *this;
-    swap(*this, other);
+    swap(other);
+    return *this;
 }
 
 template<class T>
 Matrix<T>::Matrix(Matrix<T> &&other) noexcept {
     if (this == &other)
         return;
-    swap(*this, other);
+    swap(other);
 }
 
 template<class T>
@@ -117,7 +140,7 @@ Matrix<T>::Matrix(T **_data, size_t _rows, size_t _cols) {
 
 template<class T>
 Matrix<T> Matrix<T>::transposed() {
-    Matrix result(rows, cols);
+    Matrix result(cols, rows);
     for (size_t i = 0; i < rows; ++i)
         for (size_t j = 0; j < cols; ++j)
             result.data[j][i] = data[i][j];
@@ -130,8 +153,7 @@ Matrix<T> Matrix<T>::operator-(const Matrix &other) {
     assert(cols == other.cols);
     Matrix result(rows, cols);
     for (size_t i = 0; i < rows; ++i)
-        for (size_t j = 0; j < cols; ++j)
-            result[i][j] = data[i][j] - other.data[i][j];
+        result[i] = data[i] - other.data[i];
     return result;
 }
 
@@ -141,8 +163,7 @@ Matrix<T> Matrix<T>::operator+(const Matrix &other) {
     assert(cols == other.cols);
     Matrix result(rows, cols);
     for (size_t i = 0; i < rows; ++i)
-        for (size_t j = 0; j < cols; ++j)
-            result[i][j] = data[i][j] + other.data[i][j];
+        result[i] = data[i] + other.data[i];
     return result;
 }
 
@@ -152,25 +173,14 @@ Matrix<T> Matrix<T>::operator*(const Matrix &other) {
     assert(cols == other.cols);
     Matrix result(rows, cols);
     for (size_t i = 0; i < rows; ++i)
-        for (size_t j = 0; j < cols; ++j)
-            result[i][j] = data[i][j] * other.data[i][j];
-    return result;
-}
-
-template<class T>
-Matrix<T> Matrix<T>::operator*(const T &other) {
-    Matrix result(*this);
-    for (size_t i = 0; i < rows; ++i)
-        for (size_t j = 0; j < cols; ++j)
-            result[i][j] = data[i][j] * other;
+        result[i] = data[i] * other.data[i];
     return result;
 }
 
 template<class T>
 Matrix<T>& Matrix<T>::operator*=(const T& other) {
     for (size_t i = 0; i < rows; ++i)
-        for (size_t j = 0; i < cols; ++j)
-            data[i][j] = data[i][j] * other;
+        data[i] = data[i] * other;
     return *this;
 }
 
@@ -184,40 +194,23 @@ Matrix<T> operator*(const Matrix<T> &a, const T &b) {
 }
 
 template<class T>
-T Matrix<T>::determinant() const {
-    assert(rows == cols);
-    T n1;
-    T n2;
-    T det(1);
-    T total(1);
-    size_t index;
-    size_t _size = size().first;
-
-    Vector<T> temp(_size + 1);
-    for (size_t i = 0; i < _size; ++i) {
-        index = i;
-        while (index < _size && data[index][i] == T(0))
-            ++index;
-        if (index == _size)
-            continue;
-        if (index != i) {
-            for (size_t j = 0; j < _size; ++j)
-                std::swap(data[index][j], data[i][j]);
-            det = det * ((index - i) % 2 == 0 ? T(1) : T(-1));
-        }
-        for (size_t j = 0; j < _size; ++j)
-            temp[j] = data[i][j];
-        for (size_t j = i + 1; j < _size; ++j) {
-            n1 = temp[i];
-            n2 = data[j][i];
-            for (size_t k = 0; k < _size; ++k)
-                data[j][k] = (n1 * data[j][k]) - (n2 * temp[k]);
-            total *= n1;
-        }
+T Matrix<T>::_determinant(const Matrix& mtx) const {
+    assert(mtx.rows == mtx.cols);
+    if (mtx.rows == 1)
+        return mtx[0][0];
+    if (mtx.rows == 2)
+        return mtx[0][0] * mtx[1][1] - mtx[0][1] * mtx[1][0];
+    T result(0);
+    for (size_t i = 0; i < mtx.rows; ++i) {
+        T mul = i % 2 == 0 ? 1 : -1;
+        result += mul * mtx.data[i][0] * _determinant(mtx.downsized(i, 0));
     }
-    for (size_t i = 0; i < _size; ++i)
-        det *= data[i][i];
-    return det / total;
+    return result;
+}
+
+template<class T>
+T Matrix<T>::determinant() const {
+    return _determinant(*this);
 }
 
 template<class T>
@@ -226,6 +219,7 @@ Vector<T> Matrix<T>::getDiag() const {
     Vector<T> result(elems_in_diag);
     for (size_t i = 0; i < elems_in_diag; ++i)
         result[i] = data[i][i];
+    return result;
 }
 
 template<class T>
@@ -250,8 +244,8 @@ Matrix<T>::Matrix(std::initializer_list<Vector<T>> initializerList) {
     rows = initializerList.size();
     cols = initializerList.begin()->size();
     data = Vector<Vector<T>>(rows);
-    for (auto it_src = initializerList.begin(),
-                 it_dst = data.begin(); it_src < initializerList.end(); ++it_src, ++it_dst)
+    auto it_dst = data.begin();
+    for (auto it_src = initializerList.begin(); it_src < initializerList.end(); ++it_src, ++it_dst)
         *it_dst = *it_src;
 }
 
@@ -290,6 +284,155 @@ Vector<T>& Matrix<T>::operator[](size_t idx) {
 template<class T>
 std::pair<size_t, size_t> Matrix<T>::size() const {
     return std::make_pair(rows, cols);
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator-=(const Matrix &other) {
+    for (size_t i = 0; i < rows; ++i)
+        data[i] -= other;
+    return *this;
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator+=(const Matrix &other) {
+    for (size_t i = 0; i < rows; ++i)
+        data[i] += other;
+    return *this;
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator*=(const Matrix &other) {
+    for (size_t i = 0; i < rows; ++i)
+        data[i] *= other;
+    return *this;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator-(const T &other) {
+    Matrix result(*this);
+    for (size_t i = 0; i < rows; ++i)
+        result.data[i] -= other;
+    return result;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator+(const T &other) {
+    Matrix result(*this);
+    for (size_t i = 0; i < rows; ++i)
+        result.data[i] += other;
+    return result;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator*(const T &other) {
+    Matrix result(*this);
+    for (size_t i = 0; i < rows; ++i)
+        result.data[i] *= other;
+    return result;
+}
+
+template<class U>
+Matrix<U> operator*(const U &a, const Matrix<U> &b) {
+    Matrix result(b);
+    for (size_t i = 0; i < b.rows; ++i)
+        result.data[i] *= a;
+    return result;
+}
+
+template<class U>
+Matrix<U> operator-(const U &a, const Matrix<U> &b) {
+    Matrix result(b);
+    for (size_t i = 0; i < b.rows; ++i)
+        result.data[i] *= -1, result.data[i] += a;
+    return result;
+}
+
+template<class U>
+Matrix<U> operator+(const U &a, const Matrix<U> &b) {
+    Matrix result(b);
+    for (size_t i = 0; i < b.rows; ++i)
+        result.data[i] += a;
+    return result;
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator+=(const T &other) {
+    for (size_t i = 0; i < rows; ++i)
+        data[i] += other;
+    return *this;
+}
+
+template<class T>
+Matrix<T>& Matrix<T>::operator-=(const T &other) {
+    for (size_t i = 0; i < rows; ++i)
+        data[i] -= other;
+    return *this;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::operator*(const Vector<T> &other) {
+    if (other.get_format() == Row) {
+        assert(cols == 1);
+        Matrix result(rows, other.size());
+        for (size_t i = 0; i < result.rows; ++i) {
+            for (size_t j = 0; j < result.cols; ++j) {
+                result[i][j] += data[i][0] * other[j];
+            }
+        }
+        return result;
+    }
+    assert(cols == other.size());
+    Matrix result(rows, 1);
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < other.size(); ++j)
+            result.data[i][0] += data[i][j] * other[j];
+    }
+    return result;
+}
+
+template<class U>
+Matrix<U> operator*(const Vector<U> &a, const Matrix<U> &b) {
+    if (a.get_format() == Row) {
+        assert(a.size() == b.rows);
+        Matrix<U> result(1, b.cols);
+        for (size_t i = 0; i < result.cols; ++i)
+            for (size_t j = 0; j < b.rows; ++j)
+                result[0][i] += a[j] * b[j][i];
+        return result;
+    }
+    assert(b.rows == 1);
+    Matrix<U> result(a.size(), b.cols);
+    for (size_t i = 0; i < result.rows; ++i)
+        for (size_t j = 0; j < result.cols; ++j)
+            result[i][j] += a[i] * b[0][j];
+    return result;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::dot(const Matrix &other) {
+    assert(cols == other.rows);
+    Matrix result(rows, other.cols);
+    for (size_t i = 0; i < result.rows; ++i)
+        for (size_t j = 0; j < result.cols; ++j) {
+            for (size_t k = 0; k < cols; ++k)
+                result[i][j] += data[i][k] * other[k][j];
+        }
+    return result;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::downsized(size_t row, size_t col) const {
+    Matrix res(*this);
+    assert(res.rows != 0);
+    for (size_t i = row; i < rows - 1; ++i)
+        res.data[i] = res.data[i + 1];
+    --res.rows;
+    assert(res.rows != 0);
+    for (size_t j = col; j < cols - 1; ++j)
+        for (size_t i = 0; i < res.rows; ++i)
+            res.data[i][j] = res.data[i][j + 1];
+    --res.cols;
+    return res;
 }
 
 #endif //TECHNOPARK_CPP_TASK_2_MATRIX_HPP
---
+++ tests/test_matrix.cpp
@@ -0,0 +1,261 @@
+#include <gtest/gtest.h>
+
+#include "matrix.hpp"
+
+#define EXPECT_EQ_MATRIX(a, b) ASSERT_EQ(a.size().first, b.size().first); \
+ASSERT_EQ(a.size().second, b.size().second);                          \
+for (size_t i = 0; i < a.size().first; ++i)                               \
+for (size_t j = 0; j < a.size().second; ++j)                              \
+EXPECT_EQ(a[i][j], b[i][j]);
+
+#define EXPECT_EQ_VECTOR(a, b) ASSERT_EQ(a.size(), b.size()); \
+EXPECT_EQ(a.get_format(), b.get_format());                                \
+for (size_t i = 0; i < a.size(); ++i)                         \
+EXPECT_EQ(a[i], b[i]);
+
+TEST(Build_Matrix, From_size) {
+    Matrix<int> mtx(10, 9);
+    EXPECT_EQ(mtx.size().first, 10);
+    EXPECT_EQ(mtx.size().second, 9);
+    for (size_t i = 0; i < mtx.size().first; ++i)
+        for (size_t j = 0; j < mtx.size().second; ++j)
+            EXPECT_EQ(mtx[i][j], 0);
+}
+
+TEST(Build_Matrix, From_initializer_list) {
+    Matrix<int> matrix = {
+            {1, 2, 3},
+            {1, 2, 3},
+            {1, 2, 3}
+    };
+    int mtx[3][3] = {
+            {1, 2, 3},
+            {1, 2, 3},
+            {1, 2, 3}
+    };
+
+    for (size_t i = 0; i < 3; ++i)
+        for (size_t j = 0; j < 3; ++j)
+            EXPECT_EQ(matrix[i][j], mtx[i][j]);
+}
+
+TEST(Build_Matrix, From_vec_of_vec) {
+    Vector<Vector<int>> vec = {
+            {1, 2, 3},
+            {1, 2, 3},
+            {1, 2, 3}
+    };
+    Matrix<int> matrix(vec);
+    ASSERT_EQ(matrix.size().first, 3);
+    ASSERT_EQ(matrix.size().second, 3);
+    for (size_t i = 0; i < 3; ++i)
+        for (size_t j = 0; j < 3; ++j)
+            EXPECT_EQ(matrix[i][j], vec[i][j]);
+}
+
+TEST(Build_Matrix, From_initializer_of_vec) {
+    int mtx[3][3] = {
+            {1, 2, 3},
+            {1, 2, 3},
+            {1, 2, 3}
+    };
+    Matrix<int> matrix({
+                Vector({1, 2, 3}),
+                Vector({1, 2, 3}),
+                Vector({1, 2, 3})
+    });
+    ASSERT_EQ(matrix.size().first, 3);
+    ASSERT_EQ(matrix.size().second, 3);
+    for (size_t i = 0; i < 3; ++i)
+        for (size_t j = 0; j < 3; ++j)
+            EXPECT_EQ(matrix[i][j], mtx[i][j]);
+}
+
+TEST(Build_Matrix, From_other_matrix) {
+    Matrix<int> matrix({Vector({1, 2, 3}),
+                        Vector({1, 2, 3}),
+                        Vector({1, 2, 3})
+                       });
+    Matrix<int> matrix1(matrix);
+    EXPECT_EQ_MATRIX(matrix, matrix1);
+}
+
+TEST(Build_Matrix, From_other_matrix_assign) {
+    Matrix<int> matrix({Vector({1, 2, 3}),
+                        Vector({1, 2, 3}),
+                        Vector({1, 2, 3})
+                       });
+    Matrix<int> matrix1 = matrix;
+    EXPECT_EQ_MATRIX(matrix, matrix1);
+}
+
+TEST(Build_Matrix, From_move_matrix) {
+    Matrix<int> matrix({Vector({1, 2, 3}),
+                        Vector({1, 2, 3}),
+                        Vector({1, 2, 3})
+                       });
+    Matrix<int> res(matrix);
+    Matrix<int> matrix1(std::move(matrix));
+    EXPECT_EQ_MATRIX(res, matrix1);
+}
+
+TEST(Build_Matrix, From_move_assign_matrix) {
+    Matrix<int> matrix({Vector({1, 2, 3}),
+                        Vector({1, 2, 3}),
+                        Vector({1, 2, 3})
+                       });
+    Matrix<int> res(matrix);
+    Matrix<int> matrix1 = std::move(matrix);
+    EXPECT_EQ_MATRIX(res, matrix1);
+}
+
+TEST(Matrix_functionality, Transpose_matrix) {
+    Matrix<int> res = {
+            {1, 2, 3},
+            {1, 2, 3}
+    };
+    Matrix<int> a = {
+            {1, 1},
+            {2, 2},
+            {3, 3}
+    };
+    Matrix<int> b = a.transposed();
+    EXPECT_EQ_MATRIX(res, b);
+}
+
+TEST(Matrix_functionality, Math_operations_with_scalars) {
+    Matrix<int> a = {
+            {1, 2, 3},
+            {1, 2, 3}
+    };
+    a -= 1;
+    Matrix<int> res = {
+            {0, 1, 2},
+            {0, 1, 2}
+    };
+    EXPECT_EQ_MATRIX(a, res);
+    a += 2;
+    res = {
+            {2, 3, 4},
+            {2, 3, 4}
+    };
+    EXPECT_EQ_MATRIX(a, res);
+    a *= 2;
+    res = {
+            {4, 6, 8},
+            {4, 6, 8}
+    };
+
+    Matrix<int> b = a - 4;
+    res = {
+            {0, 2, 4},
+            {0, 2, 4}
+    };
+    EXPECT_EQ_MATRIX(b, res);
+
+    b = a + 2;
+    res = {
+            {6, 8, 10},
+            {6, 8, 10}
+    };
+    EXPECT_EQ_MATRIX(b, res);
+
+    b = a * 2;
+    res = {
+            {8, 12, 16},
+            {8, 12, 16}
+    };
+    EXPECT_EQ_MATRIX(b, res);
+}
+
+TEST(Matrix_functionality, Math_operations_with_vectors) {
+    Vector<int> vec = {1, 2, 3};
+    Matrix<int> mtx = {
+            {1, 2},
+            {3, 4},
+            {5, 6}
+    };
+    Matrix<int> mul_mtx = vec * mtx;
+    Matrix<int> res = {
+            {22, 28}
+    };
+    EXPECT_EQ_MATRIX(res, mul_mtx);
+
+    vec.set_format(Column);
+    mtx = {
+            {1, 2, 3}
+    };
+
+    mul_mtx = mtx * vec;
+
+    res = { {14} };
+    EXPECT_EQ_MATRIX(res, mul_mtx);
+}
+
+TEST(Matrix_functionality, Math_operations_with_matrix) {
+    Matrix<int> mtx1 = {
+            {1, 2},
+            {3, 4},
+            {5, 6}
+    };
+    Matrix<int> mtx2 = {
+            {1, 2, 3},
+            {4, 5, 6}
+    };
+
+    Matrix<int> mtx3 = mtx1.dot(mtx2);
+    Matrix<int> res = {
+            {9, 12, 15},
+            {19, 26, 33},
+            {29, 40, 51}
+    };
+    EXPECT_EQ_MATRIX(mtx3, res);
+
+    Matrix<int> mtx4 = mtx1.transposed();
+    Matrix<int> mtx5 = mtx2.transposed();
+    EXPECT_EQ(mtx5.size().first, 3);
+    EXPECT_EQ(mtx5.size().second, 2);
+    Matrix<int> mtx6 = mtx4.dot(mtx5);
+    res = {
+            {22, 49},
+            {28, 64}
+    };
+    EXPECT_EQ_MATRIX(mtx6, res);
+}
+
+TEST(Matrix_functionality, Get_Projections) {
+    Matrix<int> mtx = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+    };
+    Vector<int> vec = mtx.getDiag();
+    Vector<int> res = {1, 5, 9};
+    EXPECT_EQ_VECTOR(vec, res);
+    vec = mtx.getColumn(1);
+    res = {2, 5, 8};
+    res.set_format(Column);
+    EXPECT_EQ_VECTOR(vec, res);
+    mtx[1][1] = 4;
+    EXPECT_EQ_VECTOR(vec, res);
+    vec[1] = 0;
+    EXPECT_EQ(mtx[1][1], 4);
+    vec = mtx[1];
+    res = {4, 4, 6};
+    EXPECT_EQ_VECTOR(vec, res);
+    mtx[1][1] = 5;
+    EXPECT_EQ_VECTOR(vec, res);
+    vec[1] = 0;
+    EXPECT_EQ(mtx[1][1], 5);
+}
+
+
+TEST(Matrix_Determinant, Default) {
+    Matrix<int> matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 1, 9}
+    };
+    int det = matrix.determinant();
+    EXPECT_EQ(det, -42);
+}
